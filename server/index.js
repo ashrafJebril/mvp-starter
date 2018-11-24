6d59d0f5e843c946866b5a4bcb6f232139118c13
ashraf <ashrafjebril9@gmail.com>
2018-11-24 15:15:04 +0200

    first

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.get('/items', function (req, res) {
   });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT||3000, function() {
   console.log('listening on port 3000!');
 });
 
